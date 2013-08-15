1465f01bc9c276fdb7c8282ca95bab8432183c7f
yuin <yuin@inforno.net>
2013-08-15 18:32:14 +0900

    FIXED: icon is not loaded in some cases

--- src/ib_comp_value.cpp
+++ src/ib_comp_value.cpp
@@ -37,12 +37,16 @@ const std::string* ib::CompletionString::getContextMenuPath() const { // {{{
 } // }}}
 
 Fl_RGB_Image* ib::CompletionString::loadIcon(const int size) { // {{{
-  ib::oschar ospath[IB_MAX_PATH];
-  ib::platform::utf82oschar_b(ospath, IB_MAX_PATH, icon_file_.empty() ? value_.c_str() : icon_file_.c_str());
-  if(ib::platform::is_path(ospath)){
-    return ib::IconManager::inst()->readFileIcon(icon_file_.empty() ? value_.c_str() : icon_file_.c_str(), size);
+  if(icon_file_.empty()){
+    return ib::IconManager::inst()->getAssociatedIcon(value_.c_str(), size, false);
   }else{
-    return 0;
+    ib::oschar ospath[IB_MAX_PATH];
+    ib::platform::utf82oschar_b(ospath, IB_MAX_PATH, icon_file_.c_str());
+    if(ib::platform::is_path(ospath)){
+      return ib::IconManager::inst()->readFileIcon(icon_file_.empty() ? value_.c_str() : icon_file_.c_str(), size);
+    }else{
+      return 0;
+    }
   }
 } // }}}
 // }}}
